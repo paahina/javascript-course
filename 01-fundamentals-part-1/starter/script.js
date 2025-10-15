c59c1513ba7203743fd1f993bda72ff5636d33f3
paahina <pagepoako@gmail.com>
2025-10-15 23:40:10 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,75 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("=== VARIABLES ===");
+//Let
+// let firstName = "Jonas";
+// console.log(firstName);
+
+// let age = 30;
+// console.log(age);
+// age = 50;
+// console.log(age);
+
+//Const
+// const birthYear = 19
+// console.log(birthYear);
+
+// const PI = 3.1415;
+//  console.log(PI);
+
+//Var
+// var job = "programmer";
+// job = "teacher";
+// console.log(job);
+
+//Best Practice is to use const by default
+
+/*
+const country = "Portugal";
+const language = "Protugueses";
+const population = 10;
+*/
+
+//Use let only when you need to change the value:
+
+/*
+let age = 20;
+age = 26; this will change
+ */
+
+console.log("=== DATA TYPES ===");
+
+// Number (integers and decimals)
+let age = 23;
+console.log(age);
+console.log(typeof age);
+
+//Stirng (text in quotes)
+let firstName = "Jonas";
+console.log(firstName);
+console.log(typeof firstName);
+
+//Boolean (true or false)
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun); // "boolean"
+
+// Undefined (no value assigned yet)
+let year;
+console.log(year);
+console.log(typeof year);
+
+//JavaScript variables can change types:
+
+//start as a number
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//change to a string
+dynamicVariable = "Now I'm a string!";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//change to a boolean
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
